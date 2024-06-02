3c82d6a4cbb8e35fc8a0ae85d30d963ab961ee3f
Jorge Jimenez <jjimenezdiaz@quental.com>
2024-06-02 23:49:42 +0200

    feat: init app

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './assets/styles/main.scss'
+
+function App() {
+  return (
+    <div className='app-content'>
+      <header>
+        Header here...
+      </header>
+
+      <main>
+        Content here...
+      </main>
+
+      <footer>
+        Footer Here...
+      </footer>
+    </div>
+  )
+}
+
+export default App
