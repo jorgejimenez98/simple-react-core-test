a3180233c78be2414faf83f59eb6d9ed6fe61860
Jorge Jimenez <jjimenezdiaz@quental.com>
2024-06-03 00:12:24 +0200

    feat: added use-effect page

--- src/components/UseEffectPage.tsx
+++ src/components/UseEffectPage.tsx
@@ -1,8 +1,23 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 const UseEffectPage: React.FC = () => {
+  const [counter, setCounter] = useState(0)
+  const [text, setText] = useState("")
+
+  const handleClick = () => {
+    setCounter(value => value += 1)
+  }
+
+  useEffect(() => {
+    setText(`The value of counter is: ${counter}`)
+  }, [counter]);
+
   return (
-    <div>UseEffectPage</div>
+    <div className="page">
+      <span>{text}</span>
+
+      <button onClick={handleClick}>SUM</button>
+    </div>
   )
 }
 
