fb48995374df0a1546b82d660922d74d892b09ce
Jorge Jimenez <jjimenezdiaz@quental.com>
2024-06-03 00:28:59 +0200

    feat: added use-callback page

--- src/components/UseCallbackPage.tsx
+++ src/components/UseCallbackPage.tsx
@@ -1,6 +1,21 @@
+import React, { useState, useCallback } from 'react';
+
 const UseCallbackPage: React.FC = () => {
+  const [counter, setCounter] = useState(0)
+
+  const increment = useCallback(() => setCounter(value => value += 1), [])
+  const decrement = useCallback(() => setCounter(value => value -= 1), [])
+
   return (
-    <div>UseCallbackPage</div>
+    <div className='page'>
+      {/* Text */}
+      <span>Counter is {counter}</span>
+
+      <div className='btns'>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      </div>
+    </div>
   )
 }
 
