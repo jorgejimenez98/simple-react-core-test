4badef1e06b8e2ff224e53f3f333a9f5d6c5ad3d
Jorge Jimenez <jjimenezdiaz@quental.com>
2024-06-03 00:23:06 +0200

    feat: added use-memo page

--- src/components/UseMemoPage.tsx
+++ src/components/UseMemoPage.tsx
@@ -1,8 +1,33 @@
+import React, { useMemo, useState } from 'react';
 
 const UseMemoPage: React.FC = () => {
+  const [numberA, setNumberA] = useState<number>(0);
+  const [numberB, setNumberB] = useState<number>(0);
+
+  const result = useMemo(() => {
+    return numberA * numberB;
+  }, [numberA, numberB]);
+
   return (
-    <div>UseMemoPage</div>
-  )
+    <div className="page">
+      {/* A input */}
+      <input
+        type="number"
+        value={numberA}
+        onChange={event => setNumberA(+event.target.value)}
+      />
+
+      {/* B input */}
+      <input
+        type="number"
+        value={numberB}
+        onChange={event => setNumberB(+event.target.value)}
+      />
+
+      {/* Text */}
+      <span>The result for {numberA} * {numberB} = {result}</span>
+    </div>
+  );
 }
 
-export default UseMemoPage
+export default UseMemoPage;
